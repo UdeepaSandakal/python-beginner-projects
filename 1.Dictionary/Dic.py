0a79411c223b32a6af60d65120f23ac7ba803b53
Udeepasd <udeepasd@gmail.com>
2022-11-22 18:05:53 +0530

    Update Dic.py
    final product of the code of CLI dictionary

--- 1.Dictionary/Dic.py
+++ 1.Dictionary/Dic.py
@@ -8,10 +8,23 @@ def translate(w):
     if w in data:
         return data[w]
     elif (len(get_close_matches(w, data.keys())) > 0):
-        return "Did you mean %s instead?" %get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %get_close_matches(w, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist.P;ease double check it."
+        else:
+            return "Your entry is wrong"
     else:
         return "The word doesn't exist.Plea se double check it."
 
 word = input("Enter word: ")
 
-print(translate(word))#testing
+output = (translate(word))
+
+if type(output) == list:
+    for item in output:
+        print("\n"+item)
+else:
+    print(output)
