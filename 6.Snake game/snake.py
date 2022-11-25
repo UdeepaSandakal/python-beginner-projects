1bb4de31fe7e1aae56f2ed5d95278e49bd8f8623
Udeepasd <udeepasd@gmail.com>
2022-11-25 21:42:48 +0530

    define the variables and classes

---
+++ 6.Snake game/snake.py
@@ -0,0 +1,30 @@
+import tkinter
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+class snake:
+    pass
+
+class food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_direction():
+    pass
+
+def game_over():
+    pass
