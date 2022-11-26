553e528c9dd209d43179d5461b5c5767d5a58c18
Udeepasd <udeepasd@gmail.com>
2022-11-26 10:42:52 +0530

    constructing food object

--- 6.Snake game/snake.py
+++ 6.Snake game/snake.py
@@ -4,18 +4,23 @@ import random
 GAME_WIDTH = 500
 GAME_HEIGHT = 500
 SPEED = 50
-SPACE_SIZE = 50
+SPACE_SIZE = 30
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "FF0000"
+FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 
-class snake:
+class Snake:
     pass
 
-class food:
-    pass
+class Food:
+    def __init__(self):
+        x = random.randint(0, int(GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, int(GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill=FOOD_COLOR, tags="food") 
 
 def next_turn():
     pass
@@ -41,6 +46,22 @@ lable.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenmmheight()
+
+#how much we gonna adjust the position of our window
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}")
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
 
