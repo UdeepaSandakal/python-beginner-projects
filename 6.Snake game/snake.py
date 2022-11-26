73b3a8ff66f013c28f6acf01fc29934faa81d010
Udeepasd <udeepasd@gmail.com>
2022-11-26 11:16:39 +0530

    first movement of snake and deleting snakes body parts

--- 6.Snake game/snake.py
+++ 6.Snake game/snake.py
@@ -49,6 +49,11 @@ def next_turn(snake, food):
 
     snake.squares.insert(0, square)
 
+    #deleting the last parts of the snake
+    del snake.coordinate[-1]
+    canvas.delete(snake.squares[-1])
+    del snake.squares[-1]
+
     window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
