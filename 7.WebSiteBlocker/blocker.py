6b4a6cc19e40501dab87449a20d3bd83704dd099
Udeepasd <udeepasd@gmail.com>
2022-11-30 18:36:22 +0530

    How to make sheduling running the script

--- 7.WebSiteBlocker/blocker.py
+++ 7.WebSiteBlocker/blocker.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime as dt
 
 hosts_temp = "hosts"
-# hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+# hosts_path = r"C:\Windows\System32\drivers\etc\hosts"                     when you want to run this script in background needs to run as in Administrative mode
 redirect = "127.0.0.1"
 website_list = ["www.pornhub.com","www.xnxx.com","www.pornhd3x.tv"]
 
@@ -27,3 +27,6 @@ while True:
                 file.truncate()                                                 #delete averything
         print("Fun hours...")
     time.sleep(5)
+
+    #if you want to run this script everytime when you log in to your machine
+    #then use task sheduler to to that
